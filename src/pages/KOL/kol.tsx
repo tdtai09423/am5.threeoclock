c7364dadeb14c4909cf25d7afbb2a942a492b637
Hung Ngo <nghung179123@gmail.com>
2025-09-03 17:36:18 +0700

    fix

--- src/pages/KOL/kol.tsx
+++ src/pages/KOL/kol.tsx
@@ -2,39 +2,39 @@ import Image from "next/image";
 
 export default function KolPage() {
   return (
-    <div className="w-full p-0 flex flex-col items-center bg-[url('/img/bg-kol.svg')]">
+    <div className="w-full overflow-x-hidden p-0 flex flex-col items-center bg-[url('/img/bg-kol.svg')] bg-top bg-no-repeat bg-[length:100%_auto]">
       {/* Top Section */}
-      <div className="w-screen max-w-none flex flex-col items-center text-center">
+      <div className="w-full max-w-none flex flex-col items-center text-center">
         {/* Main Avatar */}
-        <div className="relative mt-0 w-screen">
+        <div className="relative mt-0 w-full">
           <Image
             src="/img/dien-vien-tri-tam-large-pic.svg"
             alt="Diễn viên Trí Tâm"
             width={3000}
             height={1500}
             priority
-            unoptimized
-            style={{ width: "100vw", height: "auto" }}
+            sizes="100vw"
+            className="w-full h-auto"    // <-- không còn 100vw
           />
         </div>
       </div>
 
       {/* Bottom Section */}
-      <div className="w-screen max-w-none flex items-center justify-center min-h-[100vh] mb-10 px-4 bg-[url('/img/bg-kol.svg')] bg-no-repeat bg-center bg-cover">
-        {/* <div className="w-full"></div>
+      <div className="w-full max-w-6xl flex items-center justify-center min-h-[100vh] mb-10 px-4">
+        {/* Nếu cần ảnh nhỏ thì mở cái block dưới và bỏ cái bubble to này */}
+        {/* <div className="w-full">
           <Image
             src="/img/dien-vien-tri-tam-small-pic.svg"
             alt="Bệnh viện Nhi Đồng 1 với trang trí bóng bay"
             width={1200}
             height={1200}
             className="w-full h-auto"
-            priority={false}
           />
         </div> */}
-        <div className="relative w-[90vw] md:w-[80vw] h-[100vh]">
+        <div className="relative w-[90vw] md:w-[80vw] aspect-[16/10]">
           <Image
             src="/img/bubble.svg"
-            alt="Bệnh viện Nhi Đồng 1 với trang trí bóng bay"
+            alt="Khung thoại"
             fill
             className="object-contain object-center"
             priority={false}
