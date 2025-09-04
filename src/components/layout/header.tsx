83d98628af1a099d8f9aa816d6454d27eea63a47
tdtai09423 <tranduytai7@gmail.com>
2025-09-04 14:54:57 +0700

    fix

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -153,7 +153,7 @@ export default function Header() {
 
       {/* Mobile menu overlay */}
       {isMobileMenuOpen && (
-        <div className="md:hidden bg-white fixed inset-0 z-50 h-[20px]">
+        <div className="md:hidden bg-white fixed inset-0 z-50 h-[19px]">
           {/* backdrop */}
           <div
             className="absolute inset-0 bg-black/30"
