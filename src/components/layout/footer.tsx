cca836ad59a50d163af8f2ab0b701790da8c8516
tdtai09423 <tranduytai7@gmail.com>
2025-09-04 14:31:04 +0700

    fix

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <section className="relative flex items-center justify-center px-0 py-0 bg-transparent overflow-hidden pt-1 md:mt-40 pb-1 md:pb-10">
-      <div className="relative w-[90vw] h-[18vh] md:h-[40vh] max-w-none">
+      <div className="relative w-[100vw] h-[18vh] md:h-[40vh] max-w-none">
         {/* Mobile image */}
         <div className="absolute inset-0 md:hidden select-none">
           <Image
