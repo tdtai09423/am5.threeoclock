066d3ab81a319e626c408697360bb37ccf5914be
Hung Ngo <nghung179123@gmail.com>
2025-09-10 00:02:12 +0700

    fix thang ba may

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app';
+import { LanguageProvider } from '@/contexts/LanguageContext';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <LanguageProvider>
+      <Component {...pageProps} />
+    </LanguageProvider>
+  );
+}
